e15b2349a758780db401cddc1c1071996b728b31
Oskari Martela <omartela@c1r5p1.hive.fi>
2025-03-06 14:06:50 +0200

    Adds more tests for the main

--- Module07/ex02/src/main.cpp
+++ Module07/ex02/src/main.cpp
@@ -65,5 +65,45 @@ int main(int, char**)
         numbers[i] = rand();
     }
     delete [] mirror;
+
+    Array<int> empty_array;
+    if (empty_array.size() != 0) 
+    {
+        std::cerr << "Empty array constructor failed" << std::endl;
+        return 1;
+    }
+
+
+    Array<int> single_element(1);
+    if (single_element.size() != 1) {
+        std::cerr << "Size function failed for single-element array" << std::endl;
+        return 1;
+    }
+
+    Array<int> small_array(2);
+    if (small_array.size() != 2) {
+        std::cerr << "Size function failed for small array" << std::endl;
+        return 1;
+    }
+
+    Array<double> double_array(5);
+    double_array[0] = 3.14;  // Test for doubles
+
+    std::cout << double_array[0] << std::endl;
+
+    Array<std::string> string_array(2);
+    string_array[0] = "Hello";
+    string_array[1] = "World";
+
+    std::cout << string_array[0] << std::endl;
+    std::cout << string_array[1] << std::endl;
+
+    Array<int> zero_size_array(0);
+    if (zero_size_array.size() != 0) 
+    {
+        std::cerr << "Zero size array failed" << std::endl;
+        return 1;
+    }
+
     return 0;
 }
